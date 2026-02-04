ad1b34e7d93b8b9f0ccac70256cb8ed23e7a8eac
hjcho <hjcho@seegene.com>
2026-02-04 09:30:06 +0900

    빌드 오류 수정 (2)

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,39 +3,14 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig, swcPlugin } from 'electron-vite';
 
-const TYPEORM_DRIVERS = [
-  '@google-cloud/spanner',
-  'better-sqlite3',
-  'mysql',
-  'mysql2',
-  'oracledb',
-  'pg',
-  'pg-native',
-  'pg-query-stream',
-  'redis',
-  'sqlite3',
-  'tedious',
-  'mongodb',
-  'mssql',
-  'sql.js',
-  'hdb-pool',
-  '@sap/hana-client',
-  '@sap/hana-client/extension/Stream',
-  'typeorm-aurora-data-api-driver',
-  'ioredis',
-] as const;
-
 export default defineConfig({
   main: {
     build: {
-      externalizeDeps: {
-        include: [...TYPEORM_DRIVERS],
-      },
-      bytecode: false,
+      minify: 'esbuild',
     },
     plugins: [swcPlugin()],
   },
-  preload: { build: { bytecode: true } },
+  preload: { build: { minify: 'esbuild' } },
   renderer: {
     build: {
       rollupOptions: {
